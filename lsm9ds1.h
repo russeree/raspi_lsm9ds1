4ee56fa1053f477100dc012a030c5986952ba131
russeree <reese.russell@ymail.com>
2016-06-28 11:23:28 +0000

    Now reads ID

--- lsm9ds1.h
+++ lsm9ds1.h
@@ -12,11 +12,11 @@
 #define AG_INT_GEN_THS_X_XL   0x07   // RW 
 #define AG_INT_GEN_THS_Y_XL   0x08   // RW
 #define AG_INT_GEN_THS_Z_XL   0x09   // RW
-#define AG_INT_GEN_DUT_XL     0x0a   // RW
-#define AG_REFERENCE_G        0x0b   // RW
-#define AG_INT1_CTRL          0x0c   // RW
-#define AG_INT2_CTRL          0x0d   // RW 
-#define AG_WHO_AM_I           0x0f   // R       01101000
+#define AG_INT_GEN_DUT_XL     0x0A   // RW
+#define AG_REFERENCE_G        0x0B   // RW
+#define AG_INT1_CTRL          0x0C   // RW
+#define AG_INT2_CTRL          0x0D   // RW 
+#define AG_WHO_AM_I           0x0F   // R       01101000
 #define AG_CRTL_REG1_G        0x10   // RW
 #define AG_CTRL_REG2_G        0x11   // RW
 #define AG_CTRL_REG3_G        0x12   // RW
@@ -27,12 +27,12 @@
 #define AG_STATUS_REG_0       0x17   // R                        OUTPUT
 #define AG_OUT_X_L_G          0x18   // R                        OUTPUT
 #define AG_OUT_X_H_G          0x19   // R                        OUTPUT
-#define AG_OUT_Y_L_G          0x1b   // R                        OUTPUT
-#define AG_OUT_Y_H_G          0x1b   // R                        OUTPUT
-#define AG OUT_Z_L_G          0x1c   // R                        OUTPUT
-#define AG_OUT_Z_H_G          0x1d   // R                        OUTPUT
-#define AG_CTRL_REG4          0x1e   // RW      00111000
-#define AG_CTRL_REG5_XL       0x1f   // RW      00111000
+#define AG_OUT_Y_L_G          0x1A   // R                        OUTPUT
+#define AG_OUT_Y_H_G          0x1B   // R                        OUTPUT
+#define AG OUT_Z_L_G          0x1C   // R                        OUTPUT
+#define AG_OUT_Z_H_G          0x1D   // R                        OUTPUT
+#define AG_CTRL_REG4          0x1E   // RW      00111000
+#define AG_CTRL_REG5_XL       0x1F   // RW      00111000
 #define AG_CTRL_REG6_XL       0x20   // RW
 #define AG_CTRL_REG7_XL       0x21   // RW
 #define AG_CTRL_REG8          0x22   // RW
@@ -42,12 +42,12 @@
 #define AG_STATUS_1           0x27   // R                        OUTPUT
 #define AG_OUT_X_L_XL         0x28   // R                        OUTPUT  
 #define AG_OUT_X_H_XL         0x29   // R                        OUTPUT
-#define AG_OUT_Y_L_XL         0x2a   // R                        OUTPUT
-#define AG_OUT_Y_H_XL         0x2b   // R                        0UTPUT
-#define AG_OUT_Z_L_XL         0x2c   // R                        OUTPUT
-#define AG_OUT_Z_H_XL         0x2d   // R                        OUTPUT
-#define AG_FIFO_CTRL          0x2e   // RW
-#define AG_FIFO_SRC           0x2f   // R                        OUTPUT
+#define AG_OUT_Y_L_XL         0x2A   // R                        OUTPUT
+#define AG_OUT_Y_H_XL         0x2B   // R                        0UTPUT
+#define AG_OUT_Z_L_XL         0x2C   // R                        OUTPUT
+#define AG_OUT_Z_H_XL         0x2D   // R                        OUTPUT
+#define AG_FIFO_CTRL          0x2E   // RW
+#define AG_FIFO_SRC           0x2F   // R                        OUTPUT
 #define AG_INT_GEN_CFG_G      0x30   // RW
 #define AG_INT_GEN_THS_XH_G   0x31   // RW
 #define AG_INT_GEN_THS_XL_G   0x32   // RW
@@ -63,8 +63,8 @@
 #define MAG_OFFSET_Y_REG_L_M  0x07   // RW
 #define MAG_OFFSET_Y_REG_H_M  0x08   // RW
 #define MAG_OFFSET_Z_REG_L_M  0x09   // RW
-#define MAG_OFFSET_Z_REG_H_M  0x0a   // RW
-#define MAG_WHO_AM_I_M        0x1f   // R       00111101
+#define MAG_OFFSET_Z_REG_H_M  0x0A   // RW
+#define MAG_WHO_AM_I_M        0x0F   // R       00111101
 #define MAG_CTRL_REG1_M       0x20   // RW      00010000
 #define MAG_CTRL_REG2_M       0x21   // RW
 #define MAG_CTRL_REG3_M       0x22   // RW      00000011
@@ -73,13 +73,13 @@
 #define MAG_STATUS_REG_M      0x27   // R       00100111
 #define MAG_OUT_X_L_M         0x28   // R       00101000
 #define MAG_OUT_X_H_M         0x29   // R       00101001
-#define MAG_OUT_Y_L_M         0x2a   // R       00101010
-#define MAG_OUT_Y_H_M         0x2b   // R       00101011
-#define MAG_OUT_Z_L_M         0x2c   // R       00101100
-#define MAG_OUT_Z_H_M         0x2d   // R       00101101
+#define MAG_OUT_Y_L_M         0x2A   // R       00101010
+#define MAG_OUT_Y_H_M         0x2B   // R       00101011
+#define MAG_OUT_Z_L_M         0x2C   // R       00101100
+#define MAG_OUT_Z_H_M         0x2D   // R       00101101
 #define MAG_INT_CFG_M         0x30   // RW
 #define MAG_INT_SRC_M         0x31   // R
-#define MAG_INT_THS_L_M       0x32   // R
-#define MAG_INT_THS_H_M 
+#define MAG_INT_THS_L_M       0x32   // R#define MAG_INT_THS_H_M #endif
+#define MAG_INT_THS_H_M #endif
 
 #endif
--- main.cc
+++ main.cc
@@ -1,6 +1,7 @@
 // Reese Russell
 // LSM9DS1 I2C RasPi I2C Access
 
+#include <unistd.h>
 #include <errno.h>
 #include <stdio.h> 
 #include <stdlib.h>
@@ -11,9 +12,46 @@
 // LSM9DS1 REGISTER MAPPING
 #include "lsm9ds1.h"
 
-#define MAG_ADDRESS   (0x3C >> 1)
+#define AG_ADDRESS    (0x6B)
+#define MAG_ADDRESS   (0x1E)
 
 int main(){
+    const char * dev_name = "/dev/i2c-1";
+    char *buf;
+    buf = new char[10];
+
+    // Open up the I2C bus.
+    int file = open(dev_name, O_RDWR);
+    if (file == -1)
+    {
+        perror(dev_name);
+        exit(1);
+    }
+
+    // Specify the address of the slave device.
+    if(ioctl(file, I2C_SLAVE, MAG_ADDRESS) < 0)
+    {
+        perror("Failed to acquire bus access and/or talk to slave.\n");
+        exit(1);
+    }
+
+    // Write a byte to the slave. 
+    buf[0] = MAG_WHO_AM_I_M;
+    if (write(file, buf, 1) != 1)
+    {
+        perror("Failed to write to the i2c bus");
+        exit(1);
+    }
+
+    // Read a byte from the slave.
+    if (read(file, buf,1) != 1)
+    {
+        perror("Failed to read from the i2x bus");
+        exit(1);
+    }
+
+    printf ("Result: %02x\n", buf[0]);
+
     return 0;
 }
 
